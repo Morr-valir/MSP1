7b499dd4989a534293d8e60407833504509fa370
Matthieu Picard <77137944+Morr-valir@users.noreply.github.com>
2022-09-01 21:07:24 +0400

    update security & create data to page single

--- src/Controller/SingleItemsController.php
+++ src/Controller/SingleItemsController.php
@@ -2,21 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Produit;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SingleItemsController extends AbstractController
 {
     /**
      * Route vers la page d'un boutique du site
+     * Utilisation des annotation Entity pour la récupération des donnée du produit séléctionner
      */
     #[Route('/Detail/{id}', name: 'app_single_items')]
-    #[Entity("item", expr="repository.findOneById(id)")]
-    public function index(): Response
+    #[Entity("Produit", expr:"repository.findOneById(id)")]
+    public function index(Produit $produit): Response
     {
+        //dd($produit);
         return $this->render('single/index.html.twig', [
             'controller_name' => 'SingleItemsController',
+            'produit' => $produit,
         ]);
     }
 }
