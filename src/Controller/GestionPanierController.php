5dcc196186c76170d361632f22734487af53ad02
Matthieu Picard <77137944+Morr-valir@users.noreply.github.com>
2022-09-04 12:25:45 +0400

    update panier

--- src/Controller/GestionPanierController.php
+++ src/Controller/GestionPanierController.php
@@ -3,11 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\ProduitRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * Page d'affichage du panier & de sa gestion
